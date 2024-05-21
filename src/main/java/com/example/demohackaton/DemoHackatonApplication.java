8498b7245bebc39c89fa9e74b5e1a0029540e834
Chimichami <cesar.carranza.r@utec.edu.pe>
2024-05-21 14:28:05 -0500

    Creado?

---
+++ src/main/java/com/example/demohackaton/DemoHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHackatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHackatonApplication.class, args);
+    }
+
+}
